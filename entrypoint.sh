a97c76196c0b4b5931df8122143232c6d3d5e365
BlueEnni <enricobachus@t-online.de>
2020-08-12 11:27:34 +0200

    changed the quotations on the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Add the cronjobs
-echo '${BACKUPDENSITYCRON}/data/backup_data_MC.sh' > /etc/crontabs/root
-echo '* * * * * /data/kill-pid.sh' >> /etc/crontabs/root
-echo '* * * * * java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS ./${JARFILE} --nojline nogui &'>>/etc/crontabs/root
+echo "${BACKUPDENSITYCRON}/data/backup_data_MC.sh" > /etc/crontabs/root
+echo "* * * * * /data/kill-pid.sh" >> /etc/crontabs/root
+echo "* * * * * java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS ./${JARFILE} --nojline nogui">>/etc/crontabs/root
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to write into crontab: $status"
