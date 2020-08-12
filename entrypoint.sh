2262494b19463ad51dae8178fda692972f7d3954
BlueEnni <enricobachus@t-online.de>
2020-08-12 13:19:51 +0200

    fixed entrypoint echo commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Add the cronjobs
 echo "${BACKUPDENSITYCRON}/data/backup_data_MC.sh" > /etc/crontabs/root
-echo "java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS ./${JARFILE} --nojline nogui">> /data/java-start.sh
-echo "$(cat java-cycle.sh)">> /data/java-start.sh
+echo "\njava -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS ./${JARFILE} --nojline nogui">> /data/java-start.sh
+echo "\n$(cat java-cycle.sh)">> /data/java-start.sh
 echo "* * * * * /data/java-start.sh">> /etc/crontabs/root
 echo "* * * * * /data/kill-pid.sh">> /etc/crontabs/root
 status=$?
