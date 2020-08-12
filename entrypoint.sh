6e8e5ec13b461532a211b0696aff35b4d13ca7e3
BlueEnni <enricobachus@t-online.de>
2020-08-12 13:38:55 +0200

    now the java-start.sh will be rewritten on container start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Add the cronjobs
 echo "${BACKUPDENSITYCRON}/data/backup_data_MC.sh" > /etc/crontabs/root
-echo "\njava -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS ./${JARFILE} --nojline nogui">> /data/java-start.sh
-echo "\n$(cat java-cycle.sh)">> /data/java-start.sh
+printf "#!/bin/bash"> /data/java-start.sh
+printf "\ncd /data">> /data/java-start.sh
+printf "\njava -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS ./${JARFILE} --nojline nogui">> /data/java-start.sh
+printf "\n$(cat java-cycle.sh)">> /data/java-start.sh
 echo "* * * * * /data/java-start.sh">> /etc/crontabs/root
 echo "* * * * * /data/kill-pid.sh">> /etc/crontabs/root
 status=$?
